3aa4193f2943d49a1ce31e8438b7ded17b57b9be
Minyong Kim <ravenade88@gmail.com>
2025-02-21 18:22:03 +0900

    [정렬] 2월 21일 - Update

--- 01_정렬/필수/19636.cpp
+++ 01_정렬/필수/19636.cpp
@@ -16,30 +16,26 @@ int main() {
 	int bmr = i_before;  // 일일 기초 대사량
 	int energy_consumption;
 
-	int divide_and_floor(int a, int b) {
-		return (a < 0 && a % b != 0) ? (a / b - 1) : (a / b);
-	}
-
 	while (day--) {
 		// 기초대사량 고려x
 		expected_weight += (i - (i_before + a));
 		if (expected_weight <= 0) {
-			state = "DANGER DIET";
+			state = "Danger Diet";
 		}
 
 		// 기초대사량 고려o
 		energy_consumption = bmr + a;
 		expected_weight_bmr += (i - energy_consumption);
 		if (expected_weight_bmr <= 0 || bmr <= 0) {
-			state_bmr = "DANGER DIET";
+			state_bmr = "Danger Diet";
 		}
 		if (abs(i - energy_consumption) > t) {
-			bmr += divide_and_floor(i - energy_consumption, 2.0);
+			bmr += floor((i - energy_consumption) / 2.0);
 		}
 	}
 
 	// 출력 기초대사량 고려x
-	if (state == "DANGER DIET") {
+	if (state == "Danger Diet") {
 		cout << state << '\n';
 	}
 	else {
@@ -47,8 +43,8 @@ int main() {
 	}
 
 	// 출력 기초대사량 고려o
-	if (state_bmr == "DANGER DIET") {
-		cout << state_bmr << '\n';
+	if (state_bmr == "Danger Diet") {
+		cout << state_bmr;
 	}
 	else {
 		if (i_before - (bmr + 0) > 0) state_bmr = "YOYO";
