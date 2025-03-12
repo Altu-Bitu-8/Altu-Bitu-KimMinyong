faa53bfde1d2ffa994aa6cace03a1a2176977ac1
Minyong Kim <ravenade88@gmail.com>
2025-03-13 02:04:03 +0900

    [브루트포스] 3월 13일 - Update

--- 04_브루트포스/필수/1476.cpp
+++ 04_브루트포스/필수/1476.cpp
@@ -8,9 +8,12 @@ int main() {
 	// 입력
 	int e, s, m;
 	cin >> e >> s >> m;
+	e %= 15;
+	s %= 28;
+	m %= 19;
 
 	// 연산
-	int year;
+	int year = 0;
 	for (int i = 1; i <= MAX; i++) {
 		if (i % 15 == e && i % 28 == s && i % 19 == m) {
 			year = i;
